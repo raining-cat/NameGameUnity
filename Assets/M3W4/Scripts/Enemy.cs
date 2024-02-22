0ce2cadc4075659b5239c80242c300e0e51bba34
raining-cat <159646115+raining-cat@users.noreply.github.com>
2024-02-22 23:17:46 +0500

    module+webinar

---
+++ Assets/M3W4/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public List<Transform> PatrolPoints;
+
+    NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (_navMeshAgent.remainingDistance < _navMeshAgent.stoppingDistance)
+        {
+            _navMeshAgent.SetDestination(PatrolPoints[Random.Range(0, PatrolPoints.Count)].position);
+        }
+    }
+}
