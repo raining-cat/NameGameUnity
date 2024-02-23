dbe00b7fdbc6ab9e3f542630741a3db2464ce98d
raining-cat <159646115+raining-cat@users.noreply.github.com>
2024-02-23 17:37:00 +0500

    2module complete

---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
