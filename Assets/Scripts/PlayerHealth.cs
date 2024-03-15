39aea0cc15f11c5ff1dabe9ab97e79e638b11df0
raining-cat <159646115+raining-cat@users.noreply.github.com>
2024-03-15 15:10:16 +0500

    Module33

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public float value = 100;
+    public RectTransform valueRectTransform;
+
+    public GameObject gameplayUI;
+    public GameObject gameOverScreen;
+
+    private float _maxValue;
+
+    private void Start()
+    {
+        _maxValue = value;
+        DrawHealthBar();
+    }
 
     public void DealDamage(float damage)
     {
         value -= damage;
         if (value <= 0)
         {
-            Destroy(gameObject);
+            PlayerIsDead();
         }
+        DrawHealthBar();
+    }
+    private void PlayerIsDead()
+    {
+        gameplayUI.SetActive(false);
+        gameOverScreen.SetActive(true);
+        GetComponent<PlayerController>().enabled = false;
+        GetComponent<FireballCaster>().enabled = false;
+        GetComponent<CameraRotation>().enabled = false;
+    }
+    private void DrawHealthBar()
+    {
+        valueRectTransform.anchorMax = new Vector2(value / _maxValue, 1);
     }
 }
