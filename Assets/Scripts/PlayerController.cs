b4de2f9479cb84b08ed74579eb44c39cf7b0fdeb
raining-cat <159646115+raining-cat@users.noreply.github.com>
2024-03-17 23:48:19 +0500

    3.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     public float speed;
 
+    public Animator animator;
+
     private Vector3 _moveVector;
     private float _fallVelocity = 0;
 
@@ -20,24 +22,40 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        MovementUpdate();
+        JumpUpdate();
+    }
+
+    private void MovementUpdate()
+    {
         //Movement
         _moveVector = Vector3.zero;
+        var runDirection = 0;
+
         if (Input.GetKey(KeyCode.W))
         {
             _moveVector += transform.forward;
+            runDirection = 1;
         }
         if (Input.GetKey(KeyCode.S))
         {
             _moveVector -= transform.forward;
+            runDirection = 2;
         }
         if (Input.GetKey(KeyCode.D))
         {
             _moveVector += transform.right;
+            runDirection = 3;
         }
         if (Input.GetKey(KeyCode.A))
         {
             _moveVector -= transform.right;
+            runDirection = 4;
         }
+        animator.SetInteger("run direction", runDirection);
+    }
+    private void JumpUpdate()
+    {
         //Jump
         if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
         {
--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -8,6 +8,7 @@ public class PlayerHealth : MonoBehaviour
 
     public GameObject gameplayUI;
     public GameObject gameOverScreen;
+    public Animator animator;
 
     private float _maxValue;
 
@@ -33,6 +34,7 @@ public class PlayerHealth : MonoBehaviour
         GetComponent<PlayerController>().enabled = false;
         GetComponent<FireballCaster>().enabled = false;
         GetComponent<CameraRotation>().enabled = false;
+        animator.SetTrigger("death"); 
     }
     private void DrawHealthBar()
     {
